8f239952522f38d1a99190e38d6c4c6a9ce0b646
juntals <juntals01@gmail.com>
2013-04-24 16:55:40 +0800

    delete alamidDBSchemaReader

--- application/hooks/pagehook.php
+++ application/hooks/pagehook.php
@@ -54,7 +54,6 @@ class Pagehook {
 		require_once realpath(ALAMIDCLASSES . 'alamiddbgenerator' . EXT);
 		require_once realpath(ALAMIDLIBS . 'File_maker'. EXT);
 		require_once realpath(ALAMIDCLASSES . 'fileextension'. EXT);
-		require_once realpath(ALAMIDCLASSES . 'alamidDBSchemaReader'. EXT);
 		require_once realpath(ALAMIDCLASSES . 'almdtables'. EXT);
 		require_once realpath(ALAMIDCLASSES . 'xmlparser'. EXT);
 		require_once realpath(ALAMIDFUNCTIONS . 'json_parser_helper'. EXT);			
