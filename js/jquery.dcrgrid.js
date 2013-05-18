440a9f30c23930ef82fbeddf9a09ab2bee5f1685
Kenneth P. Vallejos <kenn_vall@yahoo.com>
2013-05-18 14:57:42 +0800

    adds more functionality on the dcr entry form

--- js/jquery.dcrgrid.js
+++ js/jquery.dcrgrid.js
@@ -71,42 +71,93 @@
 	
 	// event handlers
 	var evtAdd = function() {
+		var mButton = $('#addbtn');
 		var output = '';
-		var mParticular = $('input[name="particular"]').val();
-		var mTender = $('select[name="tender"] option:selected').text();
-		var mTenderCode = $('select[name="tender"] option:selected').val();
-		var mReferenceNo = $('select[name="refernce"] option:selected').text();
-		var mReferenceNoCode = $('select[name="refernce"] option:selected').val();
-		var mAmt = $('input[name="amount"]').val();
-		
+		var mParticular;
+		var mTender;
+		var mTenderCode;
+		var mReferenceNo;
+		var mReferenceNoCode;
+		var mAmt;
+	
 		if(mParticular == "" || mAmt == "" || mTenderCode == "")
 			return;
+		
+		// if button is in edit mode
+		if(mButton.attr('value')== 'Add') {
+			mParticular = $('input[name="particular"]').val();
+			mTender = $('select[name="tender"] option:selected').text();
+			mTenderCode = $('select[name="tender"] option:selected').val();
+			mReferenceNo = $('select[name="refernce"] option:selected').text();
+			mReferenceNoCode = $('select[name="refernce"] option:selected').val();
+			mAmt = $('input[name="amount"]').val();
 			
-		output += '<tr>';
-		output += '<td>' + mParticular + '</td>';
-		output += '<td>' + mTender + '</td>';
-		output += '<td>' + ((mReferenceNoCode!="") ? mReferenceNo : "" ) + '</td>';
-		output += '<td>' + mAmt + '</td>';
-		output += '<td><a class="EditBtn" href="#">Edit</a>&nbsp;<a class="DelBtn" href="#">Delete</a></td>';
-		output += '</tr>';
-		
-		// appends new dom
-		$('#datagrid tbody').append(output);
-		
-		// appends data
-		$('tr').last().data('testdata', mParticular);
-		
-		// event handlers
-		$('.DelBtn').unbind('click', evtDelBtn);
-		$('.EditBtn').unbind('click', evtGetData);
-		$('.DelBtn').bind('click', evtDelBtn);
-		$('.EditBtn').bind('click', evtGetData);
-		
-		// clears the entry fields
-		$('input[name="particular"]').val("");
-		$('select[name="tender"]').val("");
-		$('select[name="refernce"]').val("");
-	 	$('input[name="amount"]').val("");
+			output += '<tr>';
+			output += '<td>' + mParticular + '</td>';
+			output += '<td>' + mTender + '</td>';
+			output += '<td>' + ((mReferenceNoCode!="") ? mReferenceNo : "" ) + '</td>';
+			output += '<td>' + mAmt + '</td>';
+			output += '<td><a class="EditBtn" href="#">Edit</a>&nbsp;<a class="DelBtn" href="#">Delete</a></td>';
+			output += '</tr>';
+			
+			// appends new dom
+			$('#datagrid tbody').append(output);
+			
+			// appends data
+			$('tr').last().data('record', {'particular': mParticular, 'tender': mTenderCode, 'refno': mReferenceNoCode, 'amnt': mAmt});
+			
+			// event handlers
+			$('.DelBtn').unbind('click', evtDelBtn);
+			$('.EditBtn').unbind('click', evtEditData);
+			$('.DelBtn').bind('click', evtDelBtn);
+			$('.EditBtn').bind('click', evtEditData);
+			
+			// clears all entry fields
+			clearEntryFields();
+			
+		} else  {
+			mParticular = $('input[name="particular"]').val();
+			mTender = $('select[name="tender"] option:selected').text();
+			mTenderCode = $('select[name="tender"] option:selected').val();
+			mReferenceNo = $('select[name="refernce"] option:selected').text();
+			mReferenceNoCode = $('select[name="refernce"] option:selected').val();
+			mAmt = $('input[name="amount"]').val();
+			
+			output += '<td>' + mParticular + '</td>';
+			output += '<td>' + mTender + '</td>';
+			output += '<td>' + ((mReferenceNoCode!="") ? mReferenceNo : "" ) + '</td>';
+			output += '<td>' + mAmt + '</td>';
+			output += '<td><a class="EditBtn" href="#">Edit</a>&nbsp;<a class="DelBtn" href="#">Delete</a></td>';
+			
+			// appends new dom
+			$('#datagrid tbody tr[edited="true"]').empty().append(output);
+			
+			// appends data
+			$('tr').last().data('record', {'particular': mParticular, 'tender': mTenderCode, 'refno': mReferenceNoCode, 'amnt': mAmt});
+			
+			// event handlers
+			$('.DelBtn').unbind('click', evtDelBtn);
+			$('.EditBtn').unbind('click', evtEditData);
+			$('.DelBtn').bind('click', evtDelBtn);
+			$('.EditBtn').bind('click', evtEditData);
+			
+			// clears the entry fields
+			$('input[name="particular"]').val("");
+			$('select[name="tender"]').val("");
+			$('select[name="refernce"]').val("");
+			$('input[name="amount"]').val("");
+			
+			// resets the button's caption into "Add"
+			mButton.attr('value', 'Add');
+			// removes the "edited=true" attribute
+			$('#datagrid tbody tr[edited="true"]').removeAttr('edited');
+			
+		}
+
+		// clears current selection on the dcr details
+		clearEditedSelection();
+
+		focusToEntryField();
 		
 		// sets focus
 		$('#entryfield input[name="particular"]').focus();
@@ -115,15 +166,83 @@
 	
 	var evtDelBtn = function() {
 		var curObj = $(this);
+		var mButton = $('#addbtn');
+		
+		// removes the row
 		curObj.parent().parent().remove();
+		// resets the button's caption into "Add"
+		mButton.attr('value', 'Add');
+		// clears current selection on the dcr details
+		clearEditedSelection();
+		// clears all entry fields
+		clearEntryFields();
+		// sets focus
+		focusToEntryField();
 		
 		return false;
 	}
 	
-	var evtGetData = function() {
-		alert($(this).parent().parent().data('testdata'));
-		//alert('Hello');
+	var evtEditData = function() {
+		
+		var parentRow = $(this).parent().parent();
+		var entryRow = $('#entryfield tbody tr:eq(0)');
+		var recData = $(this).parent().parent().data('record');
+		//alert($('th:eq(1)', parentRow.parent().parent()).text());
+		//alert($(this).parent().parent().data('record').particular +  ' ' + $(this).parent().parent().data('record').tender);
+
+		/*$($('tr', parentRow.parent().parent()).css({'border':'none'}));
+		parentRow.css({'border': '1px solid #ccc'});*/
+		toggleEditedRecord(parentRow);
+		
+		// gets the current selected row and put them into the entry field for edit
+		$('input[name="particular"]', entryRow).val(recData.particular);
+		$('td select[name="tender"]', entryRow).val(recData.tender);
+		$('select[name="refernce"]', entryRow).val(recData.refno);
+		$('input[name="amount"]', entryRow).val(recData.amnt);
+		
+		// tags the current row being edited
+		//parentRow.attr('Edited', true);
+		EditRecord(parentRow);
+		
+		// setfocus to the entry field
+		// $('td input[name="particular"]', entryRow).focus();
+		focusToEntryField();
+		
 		return false;
+	}
+	
+	function focusToEntryField() {
+		$('#entryfield input[name="particular"]').focus();
+	}
+	
+	function EditRecord(obj, flag) {
+		var mButton = $('#addbtn');
+		flag = flag || true;
+		
+		if(flag) {
+			obj.attr('edited', flag);
+			mButton.attr('value', 'Edit');
+		} else {
+			mButton.attr('value', 'Add');			
+		}
+		
+	}
+	
+	function toggleEditedRecord(obj) {
+		$($('tr', obj.parent().parent()).css({'border':'none'}));
+		obj.css({'border': '1px solid #ccc'});
+	}
+	
+	function clearEditedSelection() {
+		$($('#datagrid tbody tr').css({'border':'none'}));
+	}
+	
+	function clearEntryFields() {
+		// clears the entry fields
+		$('input[name="particular"]').val("");
+		$('select[name="tender"]').val("");
+		$('select[name="refernce"]').val("");
+		$('input[name="amount"]').val("");	
 	}
 	
 })(jQuery);
