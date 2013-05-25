9a53f634d912036b984a65ffe17b31817f6b2983
Kenneth P. Vallejos <kenn_vall@yahoo.com>
2013-05-25 12:34:44 +0800

    removes some files

--- application/views/tranx/dcr_new_view.php
+++
@@ -1,8 +0,0 @@
-<?php echo form_open(base_url('tranx/dcr/validatenewdcr')); ?>
-	<input type="hidden" name="cashierid" value="<?php echo $cashierid; ?>" />
-    
-	<p><label>Tranx Date: </label><input type="text" name="trnxdate" value="<?php echo curdate(); ?>" /><?php echo form_error('tranxdate', '<span class="error">', '</span>'); ?></p>
-    <p><label>Beginning Balance: </label><input type="text" name="begbal" value="0.00" /></p>
-    <p><label>Cashier: </label><input type="text" name="cashier" value="<?php echo $cashiername; ?>" /><?php echo form_error('cashier', '<span class="error">', '</span>'); ?></p>
-    <p><input type="submit" value="Save" /></p>
-<?php echo form_close(); ?>
