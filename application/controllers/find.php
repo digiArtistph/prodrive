885ea4925de02f32f529f3e460212bc241ea5240
jun <juntals01@gmail.com>
2013-05-27 11:16:26 +0800

    added prodrivedb.sql

--- application/controllers/find.php
+++ application/controllers/find.php
@@ -26,20 +26,23 @@ class Find extends CI_Controller {
 	}
 	
 	public function autocomplete(){
-		$this->load->library('form_validation');
-		$validation = $this->form_validation;
+// 		$this->load->library('form_validation');
+// 		$validation = $this->form_validation;
 		
-		$validation->set_rules('tbl', 'Table',  'required');
-		$validation->set_rules('term', 'word',  'required');
-		$validation->set_rules('tblcln', 'Culumn',  'required');
-		if($validation->run() === FALSE) {
-			show_error('Please Check Url again');
-		}else{
+// 		$validation->set_rules('tbl', 'Table',  'required');
+// 		$validation->set_rules('term', 'word',  'required');
+// 		$validation->set_rules('tblcln', 'Culumn',  'required');
+// 		if($validation->run() === FALSE) {
+// 			show_error('Please Check Url again');
+// 		}else{
+		$this->benchmark->mark('start');
 			$this->load->model('mdl_autocomplete');
-			$temp = $this->mdl_autocomplete->findkeyword($this->input->post('tbl'), $this->input->post('tblcln') , $this->input->post('term'));
+			$temp = $this->mdl_autocomplete->findkeyword("users","username" , "j");
 			$objarr = json_encode($temp);
 			echo $objarr;
-		}
+			$this->benchmark->mark('end');
+			echo $this->benchmark->elapsed_time('start', 'end');
+// 		}
 	}
 
 }
