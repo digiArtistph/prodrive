d7fe0d88f9ec6ff0fc0f53f2edc89c8d2bf70ad0
emelie <emelienagales@gmail.com>
2013-05-30 14:29:18 +0800

    adds help system

--- application/config/config.php
+++ application/config/config.php
@@ -15,7 +15,7 @@
 |
 */
 $config['base_url']	= 'http://localhost/prodrive/';
-
+//$config['base_url']	= 'http://192.168.2.100/prodrive/';
 /*
 |--------------------------------------------------------------------------
 | Index File
