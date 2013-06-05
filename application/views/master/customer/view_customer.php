d2d83326363e2d50b1370293cc3bb847d3e8475e
jun <juntals01@gmail.com>
2013-06-05 14:47:17 +0800

    adds delete func to customer

--- application/views/master/customer/view_customer.php
+++ application/views/master/customer/view_customer.php
@@ -14,7 +14,6 @@
             	<th>Name</th>     
             	<th>Address</th>
             	<th>Phone</th>
-            	<th>Status</th>
             	<th>Action</th>
             </tr>
         </thead>
@@ -24,8 +23,7 @@
             	<td><?php if( empty($customer->fname) && empty($customer->mname) && empty($customer->lname) ){echo 'No Name Acquired';} ?><?php if(!empty($customer->fname)){  echo ucfirst($customer->fname);}?> <?php if(!empty($customer->mname)){ echo ucfirst($customer->mname[0]) . '.';}?> <?php if(!empty($customer->lname)){ echo ucfirst($customer->lname);}?></td>
                 <td><?php if( empty($customer->addr) ){echo 'No Address';}else{ echo $customer->addr;}?></td>
                 <td><?php if( empty($customer->phone) ){echo 'No Phone number';}else{ echo $customer->phone;}?></td>
-                <td><?php if ( $customer->status == 1){ echo 'Active';}else{ echo 'In Active';}?></td>
-                <td><a class="reggrideditbtn" href="<?php echo base_url() . 'master/customer/section/editcustomer/' . $customer->custid;?>">edit</a>|<a class="reggriddelbtn" href="<?php echo base_url() . 'master/customer/section/deletecustomer/' . $customer->custid;?>">delete</a></td>
+                <td><a class="reggrideditbtn" href="<?php echo base_url() . 'master/customer/section/editcustomer/' . $customer->custid;?>">edit</a>|<a class="reggriddelbtn delcust" href="#" custcode="<?php echo $customer->custid;?>">delete</a></td>
         	</tr>   
 		<?php endforeach;?>
 		</tbody>
@@ -34,5 +32,5 @@
 		<p>No Customer Added!!!</p>
 	<?php endif;?>
 	</div>
-	
+<div id="dialog-confirm" title="Delete Record!!!"><p></p></div>
 </div>
